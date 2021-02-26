176691566b89e049bee93e8a0a1dcd43135d1e35
jesper.raemaekers <76244626+jesper-raemaekers@users.noreply.github.com>
2021-02-26 23:23:15 +0100

    new method of getting values for workitem

--- polarion/workitem.py
+++ polarion/workitem.py
@@ -6,59 +6,95 @@ import requests
 import re
 from urllib.parse import urljoin
 
-class Workitem(object):
 
-    def __init__(self, polarion, project, id, uri = None):
-        self.polarion = polarion
-        self.project = project
-        self.id = id
-        self.uri = uri
+class NewWorkitem(object):
 
-        service = self.polarion.getService('Tracker')
+    def __init__(self, polarion, project, id, uri=None):
+        self._polarion = polarion
+        self._project = project
+        self._id = id
+        self._uri = uri
 
-        if self.uri:
+        service = self._polarion.getService('Tracker')
+        service_test = self._polarion.getService('TestManagement')
+
+        if self._uri:
             try:
-                self.polarion_item = service.getWorkItemByUri(self.uri)
-                self.id = self.polarion_item.id
+                self._polarion_item = service.getWorkItemByUri(self._uri)
+                self._id = self._polarion_item.id
             except:
-                raise Exception(f'Cannot find workitem {self.id} in project {self.project.id}')
+                raise Exception(
+                    f'Cannot find workitem {self._id} in project {self._project.id}')
         else:
             try:
-                self.polarion_item = service.getWorkItemById(self.project.id, self.id)
+                self._polarion_item = service.getWorkItemById(
+                    self._project.id, self._id)
             except:
-                raise Exception(f'Cannot find workitem {self.id} in project {self.project.id}')
+                raise Exception(
+                    f'Cannot find workitem {self._id} in project {self._project.id}')
 
-        if self.polarion_item:
-            self.title = self.polarion_item.title
-            self.status = self.polarion_item.status.id
-            self.severity = self.polarion_item.severity.id
-            self.priority = self.polarion_item.priority.id
-            self.type = self.polarion_item.type.id
-            self.created = self.polarion_item.created
-            self.updated = self.polarion_item.updated
-            if self.polarion_item.description:
-                self.description = self.polarion_item.description.content
-            self.author = {'name': self.polarion_item.author.name, 'email': self.polarion_item.author.email, 'id':self.polarion_item.author.id}
-            self.uri = self.polarion_item.uri
-
-            self.is_test = False
-            for field in self.polarion_item.customFields.Custom:
-                if field.key == 'testType':
-                    self.is_test = True
-
-        if self.is_test:
-            service = self.polarion.getService('TestManagement')
+        if self._polarion_item != None:
+            for attr, value in self._polarion_item.__dict__.items():
+                for key in value:
+                    setattr(self, key, value[key])
+            self._polarion_test_steps = None
             try:
-                self.polarion_test = service.getTestSteps(self.uri)
+                self._polarion_test_steps = service_test.getTestSteps(self.uri)
             except:
-                raise Exception('Cannot get test properties of object that is a test item')
-            if self.polarion_test:
+                # fail silently as there are probably not test steps for this workitem
                 pass
-            #parse test steps to some sort of value
+            self._parsed_test_steps = None
+            if self._polarion_test_steps != None:
+                if self._polarion_test_steps.keys != None and self._polarion_test_steps.steps:
+                    # oh god, parse the test steps...
+                    columns = []
+                    self._parsed_test_steps = []
+                    for col in self._polarion_test_steps.keys.EnumOptionId:
+                        columns.append(col.id)
+                    # now parse the rows
+                    for row in self._polarion_test_steps.steps.TestStep:
+                        current_row = {}
+                        for col_id in range(len(row.values.Text)):
+                            current_row[columns[col_id]
+                                        ] = row.values.Text[col_id].content
+                        self._parsed_test_steps.append(current_row)
+
+    def getDescription(self):
+        """
+        Get a comment if available. The comment may contain HTML if edited in Polarion!
+        """
+        if self.description != None:
+            return self.description.content
+        return None
+
+    def setDescription(self, description):
+        self.description = {
+            'type': 'text/html',
+            'content': description,
+            'contentLossy': False
+        }
+
+    def hasTestSteps(self):
+        if self._parsed_test_steps != None:
+            return len(self._parsed_test_steps) > 0
+        return False
+
+    def save(self):
+        updated_item = {}
+        orig_work_item = self._polarion_item.__dict__.items()
+
+        for attr, value in self._polarion_item.__dict__.items():
+            for key in value:
+                current_value = getattr(self, key)
+                if current_value != value[key]:
+                    updated_item[key] = current_value
+        if len(updated_item) > 0:
+            updated_item['uri'] = self.uri
+            service = self._polarion.getService('Tracker')
+            service.updateWorkItem(updated_item)
 
     def __repr__(self):
-        return f'Workitem ({self.type}) {self.id} ({self.title})'
+        return f'Workitem ({self.type.id}) {self._id} ({self.title})'
 
     def __str__(self):
-        return f'Workitem ({self.type}) {self.id} ({self.title})'
-
+        return f'Workitem ({self.type.id}) {self._id} ({self.title})'
