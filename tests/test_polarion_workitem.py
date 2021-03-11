d80057905fa3545c0a1285725d16ef6505319b17
jesper.raemaekers <76244626+jesper-raemaekers@users.noreply.github.com>
2021-03-11 20:13:56 +0100

    updated compare function to handle array types

--- tests/test_polarion_workitem.py
+++ tests/test_polarion_workitem.py
@@ -47,6 +47,42 @@ class TestPolarionWorkitem(unittest.TestCase):
         self.assertEqual(executed_workitem, checking_workitem,
                          msg='Workitems not identical')
 
+    def test_description_change(self):
+        executed_workitem = self.global_workitem
+        new_description = 'new description on ' + datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
+        #check that description is not the same as we're about to set
+        checking_workitem = self.checking_project.getWorkitem(
+            executed_workitem.id)
+        self.assertNotEqual(new_description, checking_workitem.getDescription(), msg='Description cannot be the same before setting')
+
+        #update description
+        executed_workitem.setDescription(new_description)
+
+        #check that the description now is the same
+        checking_workitem = self.checking_project.getWorkitem(
+            executed_workitem.id)
+        self.assertEqual(new_description, checking_workitem.getDescription(), msg='description is not the same after setting')
+        self.assertEqual(executed_workitem.getDescription(), checking_workitem.getDescription(), msg='description is not the same after setting')
+
+    def test_add_comment(self):
+        executed_workitem = self.global_workitem
+        comment_title = 'New comment title on ' + datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        comment_content = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        executed_workitem.addComment(comment_title, comment_content)
+
+        checking_workitem = self.checking_project.getWorkitem(
+            executed_workitem.id)
+        
+        found = False
+        for comment in checking_workitem.comments.Comment:
+            if comment.title == comment_title:
+                found = True
+                self.assertEqual(comment.text.content, comment_content, msg='Comment content not equal')
+        self.assertTrue(found, 'Comment title not found in checking workitem')
+        
+
+
     def test_workitem_compare(self):
         executed_workitem = self.executing_project.createWorkitem('task')
         checking_workitem = self.checking_project.getWorkitem(
