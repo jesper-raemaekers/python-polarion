185d4c941f0602b0dcda7e423364ef5192eb17c2
Florian Fetz <florian.fetz@web.de>
2021-11-05 13:08:17 +0100

    Refactored create document functions Added functions to reuse and update documents Some leftovers for renaming module -> document

--- polarion/__init__.py
+++ polarion/__init__.py
@@ -2,7 +2,9 @@ from .user import UserCreator
 from .testrun import TestrunCreator
 from .workitem import WorkitemCreator
 from .factory import addCreator
+from .document import DocumentCreator
 
 addCreator('workitem', WorkitemCreator)
 addCreator('testrun', TestrunCreator)
 addCreator('user', UserCreator)
+addCreator('document', DocumentCreator)
--- polarion/document.py
+++ polarion/document.py
@@ -1,10 +1,10 @@
 import copy
 
-from .factory import createFromUri
+from .factory import createFromUri, Creator
 
 
 class Document:
-    def __init__(self, polarion, project, uri):
+    def __init__(self, polarion, project, uri=None, location=None):
         """
         Create a Document.
         :param polarion: Polarion client object
@@ -18,6 +18,17 @@ class Document:
         if self._uri is not None:
             service = self._polarion.getService('Tracker')
             self._polarion_document = service.getModuleByUri(self._uri)
+            if self._polarion_document is not None and self._polarion_document.unresolvable:
+                raise Exception(
+                    f'Cannot find document at URI {self._uri} in project {self._project.id}')
+
+        elif location is not None:
+            service = self._polarion.getService('Tracker')
+            self._polarion_document = service.getModuleByLocation(self._project.id, location)
+            if self._polarion_document is not None and self._polarion_document.unresolvable:
+                raise Exception(
+                    f'Cannot find document at location {location} in project {self._project.id}')
+            self._uri = self._polarion_document.uri
 
         self._buildFromPolarion()
 
@@ -27,6 +38,11 @@ class Document:
             for attr, value in self._polarion_document.__dict__.items():
                 for key in value:
                     setattr(self, key, value[key])
+
+    def _reloadFromPolarion(self):
+        service = self._polarion.getService('Tracker')
+        self._polarion_document = service.geModuleByUri(self._uri)
+        self._buildFromPolarion()
 
     def getWorkItemUris(self):
         """
@@ -87,20 +103,63 @@ class Document:
         service.createDocumentCommentReply(comment_uri, self._polarion.TextType(
             content=text, type='text/html', contentLossy=False))
 
-
-
     def createModule(self, name, location, allowed_workitem_types, structure_link_role):
         allowed_workitem_ids = []
         for allowed_workitem_type in allowed_workitem_types:
             allowed_workitem_ids.append(self._polarion.EnumOptionIdType(id=allowed_workitem_type))
 
-        structure_link_role_id = self._polarion.EnumOptionIdType(id=structure_link_role)
+    def reuse(self, target_project_id, target_name, link_role, derived_fields=None):
+        """
+        Reuse this document in a different project.
 
+        :param target_project_id: The target project id
+        :param target_name: The target name
+        :param link_role: The link role
+        :param derived_fields: List of fields to be derived in the target document
+        :return: The new document
+        """
+        if derived_fields is None:
+            # TODO: Which are the default derived fields?
+            derived_fields = ['id', 'description']
         service = self._polarion.getService('Tracker')
-        service.createaModule(self._project, location, name, allowed_workitem_ids, structure_link_role_id, False, None)
+        new_uri = service.reuseModule(self._uri, target_project_id, None, target_name, link_role, None, None, derived_fields)
+        return createFromUri(self._polarion, self._project, new_uri)
+
+    def update(self, revision, auto_suspect):
+        """
+        Update a reused document to a revision of the source document.
+
+        :param revision: Source document revision
+        :param auto_suspect: If set to True, changed workitems will mark their links as suspect
+        """
+        service = self._polarion.getService('Tracker')
+        service.updateDerivedDocument(self._uri, revision, auto_suspect)
+
+    def save(self):
+        """
+        Update the document in polarion
+        """
+        updated_item = {}
+
+        for attr, value in self._polarion_document.__dict__.items():
+            for key in value:
+                current_value = getattr(self, key)
+                prev_value = getattr(self._original_polarion, key)
+                if current_value != prev_value:
+                    updated_item[key] = current_value
+        if len(updated_item) > 0:
+            updated_item['uri'] = self._uri
+            service = self._polarion.getService('Tracker')
+            service.updateModule(updated_item)
+            self._reloadFromPolarion()
 
     def __repr__(self):
         return f'Polarion document {self.title} in {self.moduleFolder}'
 
     def __str__(self):
         return f'Polarion document {self.title} in {self.moduleFolder}'
+
+
+class DocumentCreator(Creator):
+    def createFromUri(self, polarion, project, uri):
+        return Document(polarion, project, uri)
