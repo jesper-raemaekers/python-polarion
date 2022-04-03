7962d698ce053baf7485a34457c52b13a6199c7b
jesper.raemaekers <76244626+jesper-raemaekers@users.noreply.github.com>
2022-04-03 09:17:13 +0200

    Added HTML parser to get links from HTML code. #54

--- polarion/utils.py
+++ polarion/utils.py
@@ -1,7 +1,47 @@
 import re
+from abc import ABC
+from html.parser import HTMLParser
+from polarion.project import Project
 
 
-def clean_html(raw_html):
+class DescriptionParser(HTMLParser, ABC):
+
+    def __init__(self, polarion_project: Project = None):
+        super(DescriptionParser, self).__init__()
+        self._polarion_project = polarion_project
+        self._data = ''
+
+    @property
+    def data(self):
+        return self._data
+
+    def reset(self):
+        super(DescriptionParser, self).reset()
+        self._data = ''
+
+    def handle_data(self, data):
+        self._data += data
+
+    def handle_starttag(self, tag, attrs):
+        # parse attributes to dict
+        attributes = {}
+        for attribute, value in attrs:
+            attributes[attribute] = value
+
+        if tag == 'span' and 'class' in attributes:
+            if attributes['class'] == 'polarion-rte-link':
+                self._handle_polarion_rte_link(attributes)
+
+    def _handle_polarion_rte_link(self, attributes):
+        if attributes['data-option-id'] == 'short' or (
+                attributes['data-option-id'] == 'long' and self._polarion_project is None):
+            self._data += attributes['data-item-id']
+        else:
+            linked_item = self._polarion_project.getWorkitem(attributes['data-item-id'])
+            self._data += str(linked_item)
+
+
+def strip_html(raw_html):
     """
     Strips all HTML tags from HTML code leaving only plain text with no formatting.
     :param raw_html: HTML string
--- tests/test_polarion_utils.py
+++ tests/test_polarion_utils.py
@@ -8,6 +8,6 @@ class TestPolarionUtils(unittest.TestCase):
         core_text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu libero ipsum. Nullam eget augue'
         html_text = f'<p><img src="bla"/>{core_text}</p></br>'
 
-        clean_text = clean_html(html_text)
+        clean_text = strip_html(html_text)
 
         self.assertEqual(core_text, clean_text, msg="Cleaned text was not equal to expected core text")
