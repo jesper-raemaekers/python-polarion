4ef4e773d5c3dcb0f84cd932b3d748d8669f425e
jesper.raemaekers <76244626+jesper-raemaekers@users.noreply.github.com>
2022-04-16 10:15:44 +0200

    Preparations for release 0.4.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="polarion",
-    version="0.3.2",
+    version="0.4.0",
     author="Jesper Raemaekers",
     author_email="j.raemaekers@relek.nl",
     description="Polarion client for Python",
