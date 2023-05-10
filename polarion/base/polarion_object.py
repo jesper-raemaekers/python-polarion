62f4ce98a734d5b1927c43627ef1cb6be89c9eac
nunobrum <nuno.brum@gmail.com>
2023-05-10 10:15:29 +0200

    Importing latest changes from the nunobrum python-polarion repository

--- polarion/base/polarion_object.py
+++ polarion/base/polarion_object.py
@@ -2,11 +2,21 @@ class PolarionObject(object):
     def __init__(self, polarion, project, id=None, uri=None):
         self._polarion = polarion
         self._project = project
-        self._id = id
-        self._uri = uri
+        self._id = id  # the id of the object is normally set on the _buildWorkitemFromPolarion method
+        self._uri = uri  # the uri of the object is normally set on the _buildWorkitemFromPolarion method
 
     def _reloadFromPolarion(self):
         raise NotImplementedError
 
     def save(self):
         raise NotImplementedError
+
+    def __getattr__(self, name):
+        if name in self.__dict__:
+            return self.__dict__[name]
+        elif name == 'id':
+            return self._id
+        elif name == 'uri':
+            return self._uri
+        else:
+            super().__getattribute__(name)
