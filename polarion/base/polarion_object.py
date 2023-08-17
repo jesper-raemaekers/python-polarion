6ba7340b2cca6ecefb4cc68ab46523edcc93880d
Nuno Brum <nuno.brum@gmail.com>
2023-08-17 13:38:36 +0200

    Updates from the original repository

--- polarion/base/polarion_object.py
+++ polarion/base/polarion_object.py
@@ -2,8 +2,8 @@ class PolarionObject(object):
     def __init__(self, polarion, project, id=None, uri=None):
         self._polarion = polarion
         self._project = project
-        self._id = id  # the id of the object is normally set on the _buildWorkitemFromPolarion method
-        self._uri = uri  # the uri of the object is normally set on the _buildWorkitemFromPolarion method
+        self._id = id
+        self._uri = uri
 
     def _reloadFromPolarion(self):
         raise NotImplementedError
