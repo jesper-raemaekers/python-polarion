7155a2a3e4cf3b8afc311728dfc595c68f46aaf0
Tristan FAURE <faure.tristan@gmail.com>
2023-10-22 03:44:12 -0400

    First commit, add cache option to zeep and create a basic test case w… (#141)
    * First commit, add cache option to zeep and create a basic test case with no properties
    * #140 add traceability import through properties
    * add documentation and example

--- polarion/polarion.py
+++ polarion/polarion.py
@@ -2,7 +2,9 @@ import atexit
 import re
 from urllib.parse import urljoin, urlparse
 import requests
-from zeep import Client, Transport
+import tempfile
+import os
+from zeep import Client, CachingClient
 from zeep.plugins import HistoryPlugin
 
 from .project import Project
@@ -27,7 +29,7 @@ class Polarion(object):
     """
 
     def __init__(self, polarion_url, user, password=None, token=None, static_service_list=False, verify_certificate=True,
-                 svn_repo_url=None, proxy=None, request_session=None):
+                 svn_repo_url=None, proxy=None, request_session=None, cache=False):
         self.user = user
         self.password = password
         self.token = token
@@ -36,6 +38,8 @@ class Polarion(object):
         self.svn_repo_url = svn_repo_url
         self.proxy = None
         self.request_session = request_session
+        self.cache = cache
+        self.transport = None
         if proxy is not None:
             self.proxy = {
                 'http': proxy,
@@ -90,10 +94,9 @@ class Polarion(object):
         """
         if 'Session' in self.services:
             self.history = HistoryPlugin()
-            self.services['Session']['client'] = Client(
-                self.services['Session']['url'] + '?wsdl', plugins=[self.history], transport=self._getTransport())
+            self.services['Session']['client'] = self.get_client('Session',[self.history])
             if self.proxy is not None:
-                self.services['Session']['client'] .transport.session.proxies = self.proxy
+                self.services['Session']['client'].transport.session.proxies = self.proxy
             try:
                 self.sessionHeaderElement = None
                 self.sessionCookieJar = None
@@ -116,6 +119,15 @@ class Polarion(object):
         else:
             raise Exception(
                 'Cannot login because WSDL has no SessionWebService')
+    
+    def get_client(self,service,plugins=[]):
+        client = None
+        if self.cache:
+            client = CachingClient(self.services[service]['url'] + '?wsdl', plugins=plugins)
+        else:
+            client = Client(self.services[service]['url'] + '?wsdl', plugins=plugins)
+        client.transport.session.verify = self.verify_certificate
+        return client
 
     def _updateServices(self):
         """
@@ -126,8 +138,7 @@ class Polarion(object):
         for service in self.services:
             if service != 'Session':
                 if 'client' not in service:
-                    self.services[service]['client'] = Client(
-                        self.services[service]['url'] + '?wsdl', transport=self._getTransport())
+                    self.services[service]['client'] = self.get_client(service)
                 self.services[service]['client'].set_default_soapheaders(
                     [self.sessionHeaderElement])
                 if self.proxy is not None:
@@ -189,14 +200,6 @@ class Polarion(object):
             raise Exception(f'PDF not supported in this Polarion version')
         return self._PdfProperties
 
-    def _getTransport(self):
-        """
-        Gets the zeep transport object
-        """
-        transport = Transport(session=self.request_session)
-        transport.session.verify = self.verify_certificate
-        return transport
-
     def hasService(self, name: str):
         """
         Checks if a WSDL service is available
@@ -207,7 +210,7 @@ class Polarion(object):
 
     def getService(self, name: str):
         """
-        Get a WSDL service client. The name can be 'Trakcer' or 'Session'
+        Get a WSDL service client. The name can be 'Tracker' or 'Session'
         """
         # request user info to see if we're still logged in
         try:
--- polarion/xml.py
+++ polarion/xml.py
@@ -3,6 +3,7 @@ from .polarion import Polarion
 from .record import Record
 from datetime import datetime
 import logging, json
+import re
 logger = logging.getLogger(__name__)
 
 class Config:
@@ -21,11 +22,14 @@ class Config:
     TESTRUN_TYPE='testrun_type'                         # type of the test run if testrun is created. If not set 'xUnit Test Manual Upload'
     TESTRUN_COMMENT='testrun_comment'                   # test run comment to add if set.
     SKIP_MISSING_TESTCASE='skip_missing_testcase'       # if set to True, skip result on unknown test cases
+    VERIFY_CERT ='verify_cert'                          # verify or not the cert
+    USE_CACHE ='use_cache'                              # verify or not the cert
     ATTRIBUTES = [
         XML_FILE, URL, USERNAME, PASSWORD, TOKEN, 
         PROJECT_ID, TESTRUN_ID, TESTRUN_ID_GENERATOR,
-        TESTRUN_TITLE, TESTRUN_TYPE, TESTRUN_COMMENT, SKIP_MISSING_TESTCASE]
+        TESTRUN_TITLE, TESTRUN_TYPE, TESTRUN_COMMENT, SKIP_MISSING_TESTCASE, VERIFY_CERT, USE_CACHE]
     MANDATORY = [XML_FILE, URL, PROJECT_ID]  # and also either user/password or token
+    
     _classinitialised = False
 
     def __new__(cls, *args, **kwargs):
@@ -44,6 +48,10 @@ class Config:
             return 'xUnit Test Manual Upload'
         elif attribute_name==Config.SKIP_MISSING_TESTCASE:
             return False
+        elif attribute_name==Config.VERIFY_CERT:
+            return True
+        elif attribute_name==Config.USE_CACHE:
+            return False
         return None
 
     @classmethod
@@ -60,6 +68,7 @@ class Config:
         Create config from a dict
         """
         return Config(data)
+
 
     def __init__(self, data):
         """
@@ -84,6 +93,7 @@ class Config:
             else:
                 self._data[Config.TESTRUN_ID]=f'unit-{datetime.now().strftime("%Y-%m-%d-%H-%M-%S-%f")}'
         return getattr(self, Config.TESTRUN_ID)
+    
 
 class XmlParser:
     """
@@ -92,6 +102,9 @@ class XmlParser:
     TEST_SUITES='testsuites'
     TEST_SUITE='testsuite'
     TEST_CASE='testcase'
+    PROPERTIES='properties'
+    PROPERTY='property'
+    SYSOUT='system-out'
 
     @classmethod
     def parse_root(cls, xml_file):
@@ -131,6 +144,21 @@ class XmlParser:
         else:
             raise Exception(f'Unmanaged {XmlParser.TEST_SUITE} {test_suite.tag} in {parent["path"]}')
 
+    # matches expressions like [[PROPERTY|verifies=REQ-001]]
+    RE_PATTTERN = re.compile("\\[\\[PROPERTY\\|(.*)\\=(.*)\\]\\]")
+
+    @classmethod
+    def tranform_string_properties(cls, value):
+        result = [] 
+        tmp =  XmlParser.RE_PATTTERN.findall(value)
+        for res in tmp:
+            if len(res) == 2:
+                result.append({
+                    "name":res[0],
+                    "value":res[1]
+                }) 
+        return result
+
     @classmethod
     def _parse_case(cls, test_case, parent, returned_cases):
         if test_case.tag == XmlParser.TEST_CASE:
@@ -146,14 +174,26 @@ class XmlParser:
             if 'time' in test_case.attrib.keys():
                 case.update({ 'time': test_case.attrib['time'] })
             
-            # error & failure
+            # error & failure & properties
             for elem in test_case:
-                if elem.tag in ['error', 'failure']:
+                if elem.tag in ['error', 'failure','skipped']:
                     text = []
                     for attrib in ['type', 'message']:
                         if attrib in elem.attrib.keys(): text.append(elem.attrib[attrib])
-                    text.append(elem.text)
+                    if elem.text is not None:
+                        text.append(elem.text)
                     case.update({ elem.tag: '\n'.join(text)})
+                elif elem.tag == XmlParser.PROPERTIES:
+                    if "properties" not in case:
+                        case.update({'properties':[]})
+                    for property in elem:
+                        if XmlParser.PROPERTY == property.tag and 'name' in property.attrib.keys() and 'value' in property.attrib.keys():
+                            case['properties'].append({property.get('name') : property.get('value')})
+                elif elem.tag == XmlParser.SYSOUT:
+                    if "properties" not in case:
+                        case.update({'properties':[]})
+                    for property in XmlParser.tranform_string_properties(elem.text):
+                        case['properties'].append({property['name'] : property['value']})
             returned_cases.append(case)
         else:
             raise Exception(f'Unmanaged {XmlParser.TEST_CASE} {test_case.tag} in {parent["path"]}')
@@ -166,12 +206,12 @@ class XmlParser:
         if 'name' in node.attrib.keys():
             return f'{node.tag}[name={node.attrib["name"]}]'
         return node.tag
-
+    
 class Importer:
     """
     Import xml file to polarion using a config
     """
-    TEST_CASE_ID_CUSTOM_FILED='testCaseID'
+    TEST_CASE_ID_CUSTOM_FIELD='testCaseID'
     TEST_CASE_TYPE='type:testcase'
     TEST_CASE_WI_TYPE='testcase'
     TEST_CASE_WI_TITLE='title'
@@ -186,16 +226,17 @@ class Importer:
         cases=XmlParser.parse_root(config.xml_file)
 
         logger.info(f'Connection to polarion {config.url} on project {config.project_id}')
-        polarion=Polarion(polarion_url=config.url, user=config.username, password=config.password, token=config.token)
+
+        polarion=Polarion(polarion_url=config.url, user=config.username, password=config.password, token=config.token, verify_certificate=config.verify_cert, cache=config.use_cache)
         project=polarion.getProject(config.project_id)
 
         # Indexing existing cases with custom field '
-        test_cases=project.searchWorkitem(Importer.TEST_CASE_TYPE, field_list=['id', f'customFields.{Importer.TEST_CASE_ID_CUSTOM_FILED}'])
+        test_cases=project.searchWorkitem(Importer.TEST_CASE_TYPE, field_list=['id', f'customFields.{Importer.TEST_CASE_ID_CUSTOM_FIELD}'])
         test_cases_from_id={}
         for test_case in test_cases:
             if hasattr(test_case,'customFields') and hasattr(test_case.customFields,'Custom'):
                 for custom in test_case.customFields.Custom:
-                    if getattr(custom, 'key', None) == Importer.TEST_CASE_ID_CUSTOM_FILED and hasattr(custom,'value'):
+                    if getattr(custom, 'key', None) == Importer.TEST_CASE_ID_CUSTOM_FIELD and hasattr(custom,'value'):
                         test_cases_from_id[custom.value]=test_case.id
 
         # Getting or creating test run
@@ -222,16 +263,19 @@ class Importer:
             test_run.setCustomField(Importer.TEST_RUN_COMMENT_CUSTOM_FIELD, test_run._polarion.TextType(
                     content=comment, type='text/html', contentLossy=False))
 
+        # cache for work items traced
+        cache_for_workitems = {}
+
         # Filling
         logger.info('Saving results')
         for case in cases:
             if case['id'] not in test_cases_from_id.keys():
                 if config.skip_missing_testcase:
-                    logger.warn(f'Skipping case with {Importer.TEST_CASE_ID_CUSTOM_FILED} {case["id"]}')
+                    logger.warn(f'Skipping case with {Importer.TEST_CASE_ID_CUSTOM_FIELD} {case["id"]}')
                     continue
-                print(f'Creating case with {Importer.TEST_CASE_ID_CUSTOM_FILED} {case["id"]}')
+                print(f'Creating case with {Importer.TEST_CASE_ID_CUSTOM_FIELD} {case["id"]}')
                 wi_case=project.createWorkitem(workitem_type=Importer.TEST_CASE_WI_TYPE, new_workitem_fields={Importer.TEST_CASE_WI_TITLE: case['id']})
-                wi_case.setCustomField(key=Importer.TEST_CASE_ID_CUSTOM_FILED, value=case['id'])
+                wi_case.setCustomField(key=Importer.TEST_CASE_ID_CUSTOM_FIELD, value=case['id'])
             else:
                 wi_case=project.getWorkitem(test_cases_from_id[case['id']])
             test_run.addTestcase(wi_case)
@@ -246,8 +290,37 @@ class Importer:
                 test_run.records[-1].setResult(Record.ResultType.FAILED, case['failure'])
             elif 'error' in case.keys():
                 test_run.records[-1].setResult(Record.ResultType.BLOCKED, case['error'])
+            elif 'skipped' in case.keys():
+                test_run.records[-1].setResult(Record.ResultType.NOTTESTED, case['skipped'])
             else:
                 test_run.records[-1].setResult(Record.ResultType.PASSED)
+            
+            # handle traceability, because of API, traqceability must use default traceability role 
+            # and not the opposite one.
+            # traceability links are made using IDs or titles 
+            # this implementation does not allow traceability between test cases
+            if 'properties' in case.keys():
+                for property in case['properties']:
+                    for key in property.keys():
+                        linked_item = None
+                        title=property.get(key)
+                        if title in cache_for_workitems:
+                            linked_item = cache_for_workitems[title]
+                        else:
+                            try:
+                                linked_item=project.getWorkitem(property.get(key))
+                            except Exception:
+                                linked_items=project.searchWorkitem(query=f'title:{title}', field_list=['id','title'])
+                                if len(linked_items) > 0 and linked_items[0]['title']==title:
+                                    linked_item=linked_items[0]
+                                    # work item is reload to avoid isues of class not correctly loaded by search work item
+                                    linked_item=project.getWorkitem(linked_item['id'])
+                                else:
+                                    logger.error(f'impossible to link{wi_case.id} to {title}')
+                                cache_for_workitems[title] = linked_item
+                        if linked_item is not None:
+                            wi_case.addLinkedItem(linked_item, key)
+
         logger.info(f'Results saved in {config.url}/#/project/{config.project_id}/testrun?id={config.testrun_id}')
 
         return test_run
