3f07b1325b538db268ce0ad278cba21d094a3439
Rémi Heens <me@remiheens.fr>
2024-05-22 20:23:27 +0000

    Preparations for release 1.4.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="polarion",
-    version="1.3.0",
+    version="1.4.0",
     author="Jesper Raemaekers",
     author_email="j.raemaekers@relek.nl",
     description="Polarion client for Python",
